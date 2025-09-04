960cd27413130c50ae784ae4c80bbf64b90901c0
yojung51123 <ak471125@naver.com>
2025-09-04 19:56:48 +0900

    생성자,스테이터스

--- PlayerL.cpp
+++ PlayerL.cpp
@@ -1,15 +1,41 @@
+//효정
 #include <iostream>
 #include <vector>
-#include "Playerl.h"
+#include "Player.h"
+#include "Monster.h"
+#include "Inventory.h"
 
 using namespace std;
 
 //필요한거
 // 닉네임 골드 레벨 체력 MAX체력 공격 경험치 인벤토리
 
-void Player::levelUp() // 레벨업시 체력 풀 회복
+Player::Player(string name):Name(name),
+	level(1),
+	health(200), 
+	attack(30), 
+	experience(0){}
+
+
+//void useItem()
+//{
+//
+//}
+
+//레벨 체력 공격력 경험치 포인터
+
+
+//닉네임 골드 레벨 체력 MAX체력 공격 경험치 인벤토리
+void Player::displayStatus()
 {
-	health += (level * 20); // 레벨업 체력
-	attack += (level * 5); // 레벨업 공격력
+	cout << " * Character status * " << endl;
+	cout << "======================" << endl;
+	cout << "닉네임 : " << name << endl;
+	cout << "Level : " << level << endl;
+	cout << "HP : " << health << endl;
+	cout << "공격력 : " << attack << endl;
+	cout << "경험치 : " << experience << endl;
+	cout << "골드 : " << gold << endl;
+	cout << "======================" << endl;
 
 }

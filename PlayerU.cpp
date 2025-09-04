7cd82eef8b86f25abc38ab14bb2628d5f8d8741f
soo-10 <uoosoo0723@gmail.com>
2025-09-04 21:04:41 +0900

    useitem정의 추가

--- PlayerU.cpp
+++ PlayerU.cpp
@@ -9,11 +9,15 @@ using namespace std;
 
 void Player::levelUp() // 레벨업시 체력 풀 회복
 {
-	MaxHealth += (level * 20); // 레벨업 체력
-	attack += (level * 5); // 레벨업 공격력
+	MaxHealth += (level * 20);  // 레벨에 20곱한 값만큼 최대체력증가
+	attack += (level * 5);     // 레벨에 5곱한 값만큼 공격력증가
 	health = MaxHealth;
-	if (level == 10) { return; }
+	if (level == 10) { return; } //만약 레벨이 10이면 돌아간다
 	else {
 		level += 1;
+		
 	}
+}
+void Player::useItem(int index) {
+
 }

08906e387d9127383fe7127595c69ec57a905c52
gamewhoim <gamewhoim@naver.com>
2025-09-04 16:34:47 +0900

    Update Monster.cpp

---
+++ Monster.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class monster
+{
+public:
+
+};
+
+class Goblin : public monster
+{
+public:
+
+};
+
+class Troll : public monster
+{
+
+};
+
+class Slime : public monster
+{
+
+};
+
+int main()
+{
+
+}

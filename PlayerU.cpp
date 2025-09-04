75f42ec5abc607752c362944ac28b1cc0855a439
jsw0416 <j93319965@gmail.com>
2025-09-04 21:10:58 +0900

    Merge pull request #11 from yojung51123/PlayerL
    1

---
+++ PlayerU.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "Player.h"
+
+using namespace std;
+
+//필요한거
+// 닉네임 골드 레벨 체력 MAX체력 공격 경험치 인벤토리
+
+void Player::levelUp() // 레벨업시 체력 풀 회복
+{
+	MaxHealth += (level * 20); // 레벨업 체력
+	attack += (level * 5); // 레벨업 공격력
+	health = MaxHealth;
+	if (level == 10) { return; }
+	else {
+		level += 1;
+	}
+}
